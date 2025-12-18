6735f49ab9cbdcaa9afb1187274497eaa04593de
TomChicken-on-github <TomChicken@qq.com>
2025-12-18 20:15:53 +0800

    fix: update version numbers in i18n.js for accuracy

--- src/i18n.js
+++ src/i18n.js
@@ -52,7 +52,7 @@ const messages = {
     },
     cta: {
         title: '立即加入 繁星之望',
-        version: '1.13-1.21.3 Java版 / 最新基岩版',
+        version: '1.13-1.21.8 Java版 / 最新基岩版',
         copy_ip: 'play.wsmcs.top',
         copied_alert: '已复制地址!',
         copy_failed_alert: '复制失败，请手动复制。',
@@ -128,7 +128,7 @@ const messages = {
     },
     cta: {
         title: 'Join Star Wish Now',
-        version: '1.13-1.21.3 Java / Latest Bedrock',
+        version: '1.13-1.21.8 Java / Latest Bedrock',
         copy_ip: 'play.wsmcs.top',
         copied_alert: 'IP Copied!',
         copy_failed_alert: 'Failed to copy. Please copy manually.',
